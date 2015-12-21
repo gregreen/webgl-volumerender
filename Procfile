aafb5a3163ed5202be5bb790ea479b90f8b0c9e9
Gregory M. Green <gregorymgreen@gmail.com>
2015-12-21 01:42:25 -0500

    Fixes to Procfile and run.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn personal:app --log-file=-
+web: gunicorn app:app --log-file=-
--- run.py
+++ run.py
@@ -2,8 +2,4 @@
 
 from app import app
 
-def main():
-    app.run(debug=True)
-
-if __name__ == '__main__':
-    main()
+app.run(debug=False)
