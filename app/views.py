dc1acba84afb26a1c21734e860f240c208f69ab4
Gregory M. Green <gregorymgreen@gmail.com>
2016-01-05 11:45:39 -0500

    Added information page on device orientation.

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/')
 def view_index():
     return render_template('volumerender.html')
+
+@app.route('/orientation')
+def view_orientation():
+    return render_template('orientation.html')
